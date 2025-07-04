132155e2bb77cf8938c4a30f6ecb923e040994b8
Matheus Silva <reis.mmatheus@gmail.com>
2025-07-04 18:19:14 +1000

    Adding project structure

---
+++ InvoiceApp/src/InvoiceApp.Application/Interface/IInvoiceInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Application.Interface
+{
+    public interface IInvoiceInterface
+    {
+    }
+}
